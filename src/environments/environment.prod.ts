3905e890262e46f93a84bdde42a765f9b862a7bc
Felipe Rivas <dfgustin.4560@unicesmag.edu.co>
2024-10-24 23:56:37 -0500

    reset deploy

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-import { QueueScheduler } from "rxjs/internal/scheduler/QueueScheduler";
-
-export const environment = {
-  production: true,  
-  apiUrl:"https://djcoffee-api.onrender.com"
-};
-
-QueueScheduler
